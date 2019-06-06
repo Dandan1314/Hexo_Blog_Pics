33031892144b3fbf4e7049712568b1f5ba7406fd
孙明喆 <sunmingzhe@sunmingzhedeMacBook-Pro.local>
2019-06-06 14:09:57 +0800

    更新 .gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    static_Dir: 'upload',
-    mongoose_ConnectString: 'mongodb://127.0.0.1:12345/Blog_Pic',
-    qiniu_AccessKey: '',
-    qiniu_SecretKey: '',
-    qiniu_Bucket: '',
-}
